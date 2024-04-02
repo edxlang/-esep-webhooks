22a2cca0fcbe500a8e7356ab90b06b22c03d5617
ed <edlang114@gmail.com>
2024-04-02 17:27:00 -0400

    commit

--- src/EsepWebhook/Function.cs
+++ src/EsepWebhook/Function.cs
@@ -1,4 +1,6 @@
+using System.Text;
 using Amazon.Lambda.Core;
+using Newtonsoft.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -7,7 +9,6 @@ namespace EsepWebhook;
 
 public class Function
 {
-    
     /// <summary>
     /// A simple function that takes a string and does a ToUpper
     /// </summary>
@@ -16,19 +17,29 @@ public class Function
     /// <returns></returns>
     public string FunctionHandler(object input, ILambdaContext context)
     {
+
         dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
+        if (!json.ContainsKey("issue"))
+        {
+            return "Not a GitHub issue";
+        }
         
+        if (json.action != "opened")
+        {
+            return "Not an issue event";
+        }
+
         string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
-        
+
         var client = new HttpClient();
         var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
         {
             Content = new StringContent(payload, Encoding.UTF8, "application/json")
         };
 
-        var response = client.Send(webRequest, Environment.GetEnvironmentVariable("SLACK_URL"));
+        var response = client.Send(webRequest);
         using var reader = new StreamReader(response.Content.ReadAsStream());
-            
+
         return reader.ReadToEnd();
     }
 }
